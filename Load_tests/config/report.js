8749e9e6da71b4a94d55e0e2390fdab053b34478
moath <moathmabosull@gmail.com>
2026-04-27 20:12:19 +0300

    Add timestamp to K6 HTML reports

--- Load_tests/config/report.js
+++ Load_tests/config/report.js
@@ -1,9 +1,20 @@
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
+function getTimestamp() {
+  const now = new Date();
+
+  const pad = (n) => n.toString().padStart(2, '0');
+
+  return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())}_` +
+         `${pad(now.getHours())}-${pad(now.getMinutes())}-${pad(now.getSeconds())}`;
+}
+
 export function generateReport(data, testName = "report") {
+  const timestamp = getTimestamp();
+
   return {
-    [`reports/${testName}.html`]: htmlReport(data),
+    [`reports/${testName}-${timestamp}.html`]: htmlReport(data),
     stdout: textSummary(data, { indent: " ", enableColors: true }),
   };
 }
